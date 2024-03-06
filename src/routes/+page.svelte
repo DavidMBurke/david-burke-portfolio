b9c1c9b37ffaea0ee1862e8354a267cb36e988ea
David Burke <burkedm94@gmail.com>
2024-03-06 12:28:07 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>My Resume</title>
+</svelte:head>
+<h1 style="width:85%; font-size: 8vh;">David Burke</h1>
+<div style="width:40%;">
+    <img src="images/photo.jpg" alt="David Burke Selfie">
+</div>
+<p>I am a 6-4 student in my first semester at MIT. I transfered from Tidewater 
+    Community College in Virginia where I earned 2 associate degrees in Computer Science
+    and Engineering. Prior to this I spent 10 years in the Navy as a Submarine Nuclear
+    Electrical Engineering Technician and as an instructor. I have a wife and a 3-year-old
+    son. 
+</p>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>Contact me</title>
+</svelte:head>
+
+<h1 style="width:85%; font-size: 8vh;">Contact</h1>
+<form action="mailto: burkedm@mit.edu" method="post" enctype="text/plain">
+    <div class="form-list">
+        <div class="form-row">
+            <label>Email: </label>
+            <input name="email" type="email"/>
+        </div>
+        <div class="form-row">
+            <label>Subject: </label>
+            <input name="subject" />
+        </div>
+        <div class="form-row">
+            <label>Message: </label>
+            <textarea name="body"> </textarea>
+        </div>
+        <button>Submit</button>
+    </div>
+</form>
