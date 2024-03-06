3065a73d63499154a15833b555e36a1d6b287571
David Burke <burkedm94@gmail.com>
2024-03-06 14:19:56 -0500

    update projects

---
+++ src/lib/Project.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let info = {};
+    export let hLevel = 2;
+    let p = info;
+</script>
+
+<style>
+    .project {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(15em, 1fr));
+    }
+</style>
+
+<div class="project">
+    <article>
+        <svelte:element this={"h" + hLevel}>{ p.title }</svelte:element>
+        <img src={p.image} alt="">
+        <p>{p.description}</p>
+    </article>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+    import Project from "$lib/Project.svelte";
+    import projects from '$lib/projects.json';
+</script>
+
+
 <svelte:head>
 	<title>My Resume</title>
 </svelte:head>
@@ -5,9 +11,16 @@
 <div style="width:40%;">
     <img src="images/photo.jpg" alt="David Burke Selfie">
 </div>
+
 <p>I am a 6-4 student in my first semester at MIT. I transfered from Tidewater 
     Community College in Virginia where I earned 2 associate degrees in Computer Science
     and Engineering. Prior to this I spent 10 years in the Navy as a Submarine Nuclear
     Electrical Engineering Technician and as an instructor. I have a wife and a 3-year-old
     son. 
 </p>
+
+<div class="projects">
+    {#each projects.slice(0,3) as p}
+        <Project info = { p } hLevel = 3 />
+    {/each}
+</div>
