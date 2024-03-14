7d4debbab0d44c1fe442fffa6d7473442c908a66
David Burke <burkedm94@gmail.com>
2024-03-14 14:57:58 -0400

    remove old code

--- static/global.js
+++ static/global.js
@@ -6,22 +6,3 @@ function $$ (selector, context = document) {
 let navLinks = $$("nav a");
 
 const ARE_WE_HOME = document.documentElement.classList.contains("home");
-
-for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    let a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add("current");
-    }
-    if (a.host != location.host) {
-        a.target = "_blank";
-    }
-    nav.append(a);
-}
-
-document.body.insertAdjacentHTML("afterbegin", `
-	`
-);
